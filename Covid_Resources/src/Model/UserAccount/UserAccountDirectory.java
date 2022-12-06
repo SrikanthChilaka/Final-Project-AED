ad9755f300d16b9c54a2b67d9de533a8f689f71a
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-06 00:11:34 -0500

    added supplier and user account

---
+++ Covid_Resources/src/Model/UserAccount/UserAccountDirectory.java
@@ -0,0 +1,14 @@
+package Model.UserAccount;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author venkat
+ */
+public class UserAccountDirectory {
+    
+}
