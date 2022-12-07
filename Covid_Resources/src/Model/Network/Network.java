0143f99ac3be59bc665db563b8fbbbaa2db9ce5f
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:35:49 -0500

    Update Network.java

--- Covid_Resources/src/Model/Network/Network.java
+++ Covid_Resources/src/Model/Network/Network.java
@@ -4,10 +4,40 @@
  */
 package Model.Network;
 
+import Model.Enterprise.EnterpriseDirectory;
+
 /**
  *
  * @author harshinichandrika
  */
 public class Network {
+    private String name;
+private EnterpriseDirectory enterpriseDirectory;
+
+public Network(){
+    enterpriseDirectory = new EnterpriseDirectory();
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public void setEnterpriseDirectory(EnterpriseDirectory enterpriseDirectory) {
+        this.enterpriseDirectory = enterpriseDirectory;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return name;
+    }
     
 }
