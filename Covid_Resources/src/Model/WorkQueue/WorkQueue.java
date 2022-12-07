4e39eb1f936e6670f976f45130af19e11b473610
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:49:51 -0500

    Update WorkQueue.java

--- Covid_Resources/src/Model/WorkQueue/WorkQueue.java
+++ Covid_Resources/src/Model/WorkQueue/WorkQueue.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model.WorkQueue;
+import java.util.ArrayList;
 
 /**
  *
  * @author harshinichandrika
  */
 public class WorkQueue {
+    private ArrayList<WorkRequest> workRequestList;
+    
+    public WorkQueue(){
+        workRequestList = new ArrayList<>();
+    }
+    public ArrayList<WorkRequest>getWorkRequestList(){
+        return workRequestList;
+    }
+    
     
 }
