30526873ef561a814fb7ef58b9bedd61f51b7fb1
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-07 00:57:18 -0500

    DoctorRole

--- Covid_Resources/src/Model/Role/DoctorRole.java
+++ Covid_Resources/src/Model/Role/DoctorRole.java
@@ -4,10 +4,26 @@
  */
 package Model.Role;
 
+import javax.swing.JPanel;
+
 /**
  *
  * @author venkat
  */
-public class DoctorRole {
+public class DoctorRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {
+        return new DoctorWorkAreaJPanel(userProcessContainer,account,(DoctorOrganization)organization,enterprise,business,network);
+    }
+    
+    
+    
+@Override
+public String toString() {
+    return "Doctor Role";
+}
+    
     
 }
+
