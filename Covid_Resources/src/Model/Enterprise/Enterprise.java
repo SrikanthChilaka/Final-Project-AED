314817e613eae3e613723c0f2526578c36fb7252
Srikanth Chilaka <chilaka.s@northeastern.edu>
2022-12-07 00:56:37 -0500

    Update Enterprise.java

--- Covid_Resources/src/Model/Enterprise/Enterprise.java
+++ Covid_Resources/src/Model/Enterprise/Enterprise.java
@@ -8,6 +8,54 @@ package Model.Enterprise;
  *
  * @author srikanthchilaka
  */
-public class Enterprise {
-    
+public abstract class Enterprise extends Organization {
+private EnterpriseType enterpriseType;
+private OrganizationDirectory organizationDirectory;
+private String zipcode;
+
+public Enterprise(String name, EnterpriseType type) {
+super(name);
+this.enterpriseType = type;
+organizationDirectory = new OrganizationDirectory();
+}
+
+public enum EnterpriseType{
+Hospital("Hospital"),
+Supplier("Supplier"),
+Logistics("Logistics"),
+Pharmacy("Pharmacy"),
+MedicalEquipmentWareHouse("MedicalEquipmentWareHouse");
+
+private String value;
+
+private EnterpriseType(String value) {
+this.value = value;
+}
+
+public String getValue() {
+return value;
+}
+
+@Override
+public String toString() {
+return value;
+}
+}
+
+public EnterpriseType getEnterpriseType() {
+return enterpriseType;
+}
+
+public OrganizationDirectory getOrganizationDirectory() {
+return organizationDirectory;
+}
+
+public String getZipcode() {
+return zipcode;
+}
+
+public void setZipcode(String zipcode) {
+this.zipcode = zipcode;
+}
+
 }
