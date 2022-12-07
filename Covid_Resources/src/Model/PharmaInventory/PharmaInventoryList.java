3985b55f4f06768df048b25c8c0e324d4e14345e
Srikanth Chilaka <chilaka.s@northeastern.edu>
2022-12-07 01:32:56 -0500

    Update PharmaInventoryList.java

--- Covid_Resources/src/Model/PharmaInventory/PharmaInventoryList.java
+++ Covid_Resources/src/Model/PharmaInventory/PharmaInventoryList.java
@@ -4,10 +4,31 @@
  */
 package Model.PharmaInventory;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author srikanthchilaka
  */
 public class PharmaInventoryList {
+    public ArrayList<PharmaInventory> getListOfMedicines() {
+        return ListOfMedicines;
+    }
+
+    public void setListOfMedicines(ArrayList<PharmaInventory> ListOfMedicines) {
+        this.ListOfMedicines = ListOfMedicines;
+    }
+    private ArrayList<PharmaInventory> ListOfMedicines;
     
+    public PharmaInventoryList(){
+        ListOfMedicines = new ArrayList<>();
+    }
+    public PharmaInventory addMedicine(){
+        PharmaInventory phInv = new PharmaInventory();
+        ListOfMedicines.add(phInv);
+        return phInv;
+    }
+    public void removeMedicine(PharmaInventory phInv){
+        ListOfMedicines.remove(phInv);
+    }
 }
