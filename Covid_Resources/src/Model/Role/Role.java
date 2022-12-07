f5a19a3c5c78e20bf692e30aee477e317be9ee8b
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-07 01:21:03 -0500

    Role

--- Covid_Resources/src/Model/Role/Role.java
+++ Covid_Resources/src/Model/Role/Role.java
@@ -4,10 +4,49 @@
  */
 package Model.Role;
 
+import javax.swing.JPanel;
+import Model.EcoSystem;
+import Model.UserAccount.UserAccount;
+import Model.Enterprise.Enterprise;
+import Model.Network.Network;
+import Model.Organization.Organization;
+import Model.UserAccount.UserAccount;
+
+
 /**
  *
  * @author venkat
  */
-public class Role {
+public abstract class Role {
+public enum RoleType{
+        Admin("Admin"), 
+        Doctor("Doctor"),
+        Logistics("Logistics"), 
+        Pharmacist("Pharmacist");
+        
+        private String value;
+        private RoleType(String value){
+            this.value = value;
+            }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+        
+        @Override 
+        public String toString(){
+            return value;
+        }
+    }
     
+    public abstract JPanel createWorkArea (JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network);
+    @Override
+    public String toString(){
+        return this.getClass().getName();
+    }
 }
+
