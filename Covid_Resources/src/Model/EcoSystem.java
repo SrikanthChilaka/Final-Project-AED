7dcee108fa489eae2ef5fae4b9ca7807871bf59f
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:32:23 -0500

    Update EcoSystem.java

--- Covid_Resources/src/Model/EcoSystem.java
+++ Covid_Resources/src/Model/EcoSystem.java
@@ -3,11 +3,68 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model;
-
+import Model.Network.Network;
+import Model.Organization.Organization;
+import Model.Role.Role;
+import Model.Role.SysAdminRole;
+import java.util.ArrayList;
 /**
  *
  * @author harshinichandrika
  */
 public class EcoSystem {
+    private static EcoSystem business;
+        private ArrayList<Network> networkList;
+
+
+
+public static EcoSystem getInstance() {
+if (business == null) {
+business = new EcoSystem();
+}
+return business;
+}
+
+
+
+public EcoSystem() {
+super(null);
+networkList = new ArrayList<>();
+}
+
+
+
+public ArrayList<Network> getNetworkList() {
+return networkList;
+}
+
+
+
+public Network createAndAddNetwork() {
+Network network = new Network();
+networkList.add(network);
+return network;
+}
+
+
+
+@Override
+public ArrayList<Role> getSupportedRole() {
+ArrayList<Role> roleList = new ArrayList<>();
+roleList.add(new SysAdminRole());
+return roleList;
+}
+
+
+
+public boolean checkIfUsernameIsUnique(String username) {
+
+
+
+if (!this.getUserAccountDirectory().checkIFUsernameIsunique(username)) {
+return false;
+}
+return true;
+}
     
 }
