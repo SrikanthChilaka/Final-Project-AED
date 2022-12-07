00e62becc49aa0b15f072deb34614c8902a7de1a
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:44:16 -0500

    Update SupplierOrganization.java

--- Covid_Resources/src/Model/Organization/SupplierOrganization.java
+++ Covid_Resources/src/Model/Organization/SupplierOrganization.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model.Organization;
-
+import Model.Role.Role;
+import Model.Role.SupplierRole;
+import java.util.ArrayList;
 /**
  *
  * @author harshinichandrika
  */
-public class SupplierOrganization {
-    
+public class SupplierOrganization extends Organization{
+public SupplierOrganization(){
+super(Organization.OrganizationType.Supplier.getValue());
+     }   
+ @Override
+ 
+ 
+ public ArrayList<Role> getSupportedRole(){
+     ArrayList<Role> roles = new ArrayList<>();
+     roles.add(new SupplierRole());
+     return roles;
+ }
 }
