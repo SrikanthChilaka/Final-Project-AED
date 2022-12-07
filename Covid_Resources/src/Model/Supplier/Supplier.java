55ecdcf66f89ec3eb37dd9a59cb23d9984b864d0
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-07 01:33:37 -0500

    Supplier

--- Covid_Resources/src/Model/Supplier/Supplier.java
+++ Covid_Resources/src/Model/Supplier/Supplier.java
@@ -9,5 +9,40 @@ package Model.Supplier;
  * @author venkat
  */
 public class Supplier {
-    
+    private String supplierName;
+    private String type;
+    private String supplierLocation;
+    private Product product;
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSupplierLocation() {
+        return supplierLocation;
+    }
+
+    public void setSupplierLocation(String supplierLocation) {
+        this.supplierLocation = supplierLocation;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
