f223790aaf4b503cad73a77af704e972262bffe7
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:39:12 -0500

    Update EquipmentHandlingOrganization.java

--- Covid_Resources/src/Model/Organization/EquipmentHandlingOrganization.java
+++ Covid_Resources/src/Model/Organization/EquipmentHandlingOrganization.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model.Organization;
+import Model.Role.MedicalEquipmentRole;
+import Model.Role.Role;
+import java.util.ArrayList;
 
 /**
  *
  * @author harshinichandrika
  */
 public class EquipmentHandlingOrganization {
+    public EquipmentHandlingOrganization(){
+        super(Organization.OrganizationType.MedicalEquipment.getValue());
+          }
+    
+    
+    @Override 
+    public ArrayList<Role> getSupportedRole(){
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new MedicalEquipmentRole());
+        return roles;
+    }
     
 }
