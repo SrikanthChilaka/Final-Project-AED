b3c1eb8c575dce60c65e38cbd3359862a23cdf9b
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:48:45 -0500

    Update SupplierWorkRequest.java

--- Covid_Resources/src/Model/WorkQueue/SupplierWorkRequest.java
+++ Covid_Resources/src/Model/WorkQueue/SupplierWorkRequest.java
@@ -8,6 +8,39 @@ package Model.WorkQueue;
  *
  * @author harshinichandrika
  */
-public class SupplierWorkRequest {
+public class SupplierWorkRequest  extends WorkRequest {
+    
+    private String medicationName;
+    private int Quantity;
+    private String deliveryTime;
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int Quantity) {
+        this.Quantity = Quantity;
+    }
+
+    public String getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(String deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+    
+    @Override
+    public String toString(){
+        return this.medicationName;
+    }
     
 }
