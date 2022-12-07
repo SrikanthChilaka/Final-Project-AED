d305e35f9006afab298f1352f0e810b4d468a1bf
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:42:24 -0500

    added organization

--- Covid_Resources/src/Model/Organization/OrganizationDirectory.java
+++ Covid_Resources/src/Model/Organization/OrganizationDirectory.java
@@ -4,10 +4,53 @@
  */
 package Model.Organization;
 
+import Model.Organization.Organization.OrganizationType;
+import java.util.ArrayList;
+
 /**
  *
  * @author harshinichandrika
  */
 public class OrganizationDirectory {
+    private ArrayList<Organization> organizationList;
+    public OrganizationDirectory () {
+    organizationList = new ArrayList<>();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+
+    public void setOrganizationList(ArrayList<Organization> organizationList) {
+        this.organizationList = organizationList;
+    }
+    
+    public Organization createOrganization(OrganizationType type){
+Organization organization = null;
+if (type.getValue().equals(OrganizationType.Doctor.getValue())){
+organization = new DoctorOrganization();
+organizationList.add(organization);
+}
+
+else if (type.getValue().equals(OrganizationType.Pharmacy.getValue())){
+organization=new PharmacyOrganization();
+organizationList.add(organization);
+}
+
+else if (type.getValue().equals(OrganizationType.Supplier.getValue())){
+organization=new SupplierOrganization();
+organizationList.add(organization);
+}
+
+else if (type.getValue().equals(OrganizationType.MedicalEquipment.getValue())){
+organization=new EquipmentHandlingOrganization();
+organizationList.add(organization);
+}
+else if (type.getValue().equals(OrganizationType.Transport.getValue())){
+organization=new TransportOrganization();
+organizationList.add(organization);
+}
+return organization;
+}
     
 }
