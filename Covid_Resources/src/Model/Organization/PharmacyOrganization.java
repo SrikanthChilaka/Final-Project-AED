292d9c9ba146f3abff1aa3675d20fdb2087d26ed
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:43:32 -0500

    Update PharmacyOrganization.java

--- Covid_Resources/src/Model/Organization/PharmacyOrganization.java
+++ Covid_Resources/src/Model/Organization/PharmacyOrganization.java
@@ -4,10 +4,42 @@
  */
 package Model.Organization;
 
+import Model.PharmaInventory.PharmaInventory;
+import Model.Role.PharmacistRole;
+import Model.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author harshinichandrika
  */
-public class PharmacyOrganization {
-    
+public class PharmacyOrganization extends Organization{
+private ArrayList<PharmaInventory> medList;
+public PharmacyOrganization()
+ {
+super(Organization.OrganizationType.Pharmacy.getValue());
+medList=new ArrayList<PharmaInventory>();
+}
+
+
+@Override
+public ArrayList<Role> getSupportedRole()
+ {
+ArrayList<Role> roles = new ArrayList<>();
+roles.add(new PharmacistRole());
+return roles;
+}
+public ArrayList<PharmaInventory> getMedList()
+ {
+return medList;
+}
+public void setMedList(ArrayList<PharmaInventory> medList)
+{
+this.medList = medList;
+}
+public void addMedicine(PharmaInventory mi)
+{
+medList.add(mi);
+}
+
 }
