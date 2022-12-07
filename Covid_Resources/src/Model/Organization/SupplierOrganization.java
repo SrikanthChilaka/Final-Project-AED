6b00771c4c4c0ca5b90237d354ffa7764d992704
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-07 01:04:31 -0500

    network, organization & workqueue added

--- Covid_Resources/src/Model/Organization/SupplierOrganization.java
+++ Covid_Resources/src/Model/Organization/SupplierOrganization.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package Model.Organization;
 
 /**
  *
  * @author harshinichandrika
  */
-public class ConfigureASystem {
+public class SupplierOrganization {
     
-   
 }
