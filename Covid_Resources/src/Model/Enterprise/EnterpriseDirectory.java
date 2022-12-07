f151d037015d16da332c9269cf71f38c8346274e
Srikanth Chilaka <chilaka.s@northeastern.edu>
2022-12-07 00:58:43 -0500

    Update EnterpriseDirectory.java

--- Covid_Resources/src/Model/Enterprise/EnterpriseDirectory.java
+++ Covid_Resources/src/Model/Enterprise/EnterpriseDirectory.java
@@ -4,10 +4,47 @@
  */
 package Model.Enterprise;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author srikanthchilaka
  */
 public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
     
+    public EnterpriseDirectory(){
+        enterpriseList = new ArrayList<>();
+        
+    }
+    public ArrayList<Enterprise> getEnterpriseList(){
+        return enterpriseList;
+        
+    }
+    public Enterprise createAndAddEnterprise(String name, Enterprise.EnterpriseType type){
+        Enterprise enterprise = null;
+        if (type == Enterprise.EnterpriseType.Hospital){
+            enterprise = new HospitalEnterprise(name);
+            enterpriseList.add(enterprise);
+            
+        }
+        else if (type == Enterprise.EnterpriseType.Supplier){
+            enterprise = new SupplierEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        else if (type == Enterprise.EnterpriseType.Logistics){
+            enterprise = new SupplierEnterprise(name);
+            enterpriseList.add(enterprise);
+    }
+        else if (type == Enterprise.EnterpriseType.MedicalEquipmentWareHouse){
+            enterprise = new MedicalEquipEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        
+        else if (type==Enterprise.EnterpriseType.Pharmacy){
+            enterprise=new PharmacyEnterprise(name);
+            enterpriseList.add(enterprise);  
+        }
+        return enterprise;
+    }
 }
