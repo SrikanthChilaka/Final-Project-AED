f23953caac8329e66bdf161767c34994ab68664d
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-07 01:18:29 -0500

    PharmacistRole

--- Covid_Resources/src/Model/Role/PharmacistRole.java
+++ Covid_Resources/src/Model/Role/PharmacistRole.java
@@ -4,10 +4,29 @@
  */
 package Model.Role;
 
+import javax.swing.JPanel;
+import Model.EcoSystem;
+import Model.Enterprise.Enterprise;
+import Model.Network.Network;
+import Model.Organization.Organization;
+import Model.Organization.PharmacyOrganization;
+import Model.UserAccount.UserAccount;
+
+
 /**
  *
  * @author venkat
  */
-public class PharmacistRole {
-    
+public class PharmacistRole extends Role {
+    @Override
+public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+return new PharmaWorkAreaJPanel(userProcessContainer, account, enterprise, (PharmacyOrganization)organization,network);
+//return new PharmaManagementJPanel();
 }
+
+@Override
+public String toString() {
+    return "Pharnacist Role";
+}
+}
+
