44d116169ee8c882cbc4241d49dfe1b04dfa2daa
Venkat2041 <114768456+Venkat2041@users.noreply.github.com>
2022-12-07 01:24:28 -0500

    SupplierRole

--- Covid_Resources/src/Model/Role/SupplierRole.java
+++ Covid_Resources/src/Model/Role/SupplierRole.java
@@ -4,10 +4,29 @@
  */
 package Model.Role;
 
+import Model.EcoSystem;
+import Model.Enterprise.Enterprise;
+import Model.Network.Network;
+import Model.Organization.Organization;
+import Model.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+
 /**
  *
  * @author venkat
  */
-public class SupplierRole {
+public class SupplierRole extends Role {
+
+  
+    @Override
+public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+return new SupplierWorkAreaJPanel(userProcessContainer,business,account,organization);
+}
+
+@Override
+public String toString() {
+    return "Support Role";
+}
     
 }
