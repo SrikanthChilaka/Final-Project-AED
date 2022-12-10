5af95080d3a972ba1e36e223063a6d75e0f0e3fc
Harshini Chandrika Dasri <dasri.h@northeastern.edu>
2022-12-10 17:03:53 -0500

    model classes modified

---
+++ Covid_Resources/src/Model/Doctor.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author harshinichandrika
+ */
+public class Doctor {
+
+    private String Name;
+    private PrescriptionList presList;   
+    public Doctor() {
+
+        presList = new PrescriptionList();
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public PrescriptionList getPresList() {
+        return presList;
+    }
+
+    public void setPresList(PrescriptionList presList) {
+        this.presList = presList;
+    }
+    public String getName() {
+        return Name;
+    }
+
+}
